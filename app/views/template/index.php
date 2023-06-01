83b7c8491c561e9a14519cc201cfcde0633a06fa
GoldOfSilver <jason27004@gmail.com>
2023-06-01 15:39:46 +0200

    Intégration et décomposition du template en partials

---
+++ app/views/template/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once '../app/views/template/partials/_head.php'; ?>
+</head>
+
+<body class="bg-gray-900 text-white font-sans leading-normal tracking-normal">
+    <?php include_once '../app/views/template/partials/_header.php'; ?>
+    <?php include_once '../app/views/template/partials/_main.php'; ?>
+    <?php include_once '../app/views/template/partials/_footer.php'; ?>
+</body>
+
+</html>
---
+++ app/views/template/partials/_main.php
@@ -0,0 +1,4 @@
+<div class="container mx-auto flex flex-wrap pt-4 pb-12 text-white">
+    <?php include_once '../app/views/template/partials/_aside.php'; ?>
+    <?php include_once '../app/views/template/partials/_main-content.php'; ?>
+</div>
